b2128374cc40f68268e4cfa5d92efc7420fcd985
Alexander R Torrijos <artorrijos@gmail.com>
2024-03-21 17:55:36 +0800

    Added test for when bad request is made

--- tests/Rainfall.Api.Tests/RainfallApiTests.cs
+++ tests/Rainfall.Api.Tests/RainfallApiTests.cs
@@ -1,4 +1,5 @@
-﻿using FastEndpoints.Testing;
+﻿using FastEndpoints;
+using FastEndpoints.Testing;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using Rainfall.Api.Endpoints.Rainfall;
@@ -21,5 +22,20 @@ public class RainfallApiTests(App app) : TestBase<App>
         var res = await app.Client.GetFromJsonAsync<RainfallReadingsResponse>("/rainfall/id/3680/readings?count=2");
 
         res.Should().BeOfType<RainfallReadingsResponse>();
+    }
+
+    [Fact]
+    public async Task Rainfall_Api_Returns_Status400_When_Count_Is_Invalid()
+    {
+        var response = await app.Client.GetAsync("/rainfall/id/3680/readings?count=999");
+
+        // Assert the status code is 400
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+
+        // Read the response body
+        var problemResponse = await response.Content.ReadFromJsonAsync<ProblemDetails>();
+
+        // Assert the response is of type ProblemDetails
+        Assert.IsType<ProblemDetails>(problemResponse);
     }
 }
